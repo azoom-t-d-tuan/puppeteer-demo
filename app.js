3e8d234b7eb94e939c711f8d9a646482e90bd9a9
Thai Doan Tuan <t.d.tuan@azoom.jp>
2020-01-22 17:07:33 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+const puppeteer = require('puppeteer')
+
+
+var app = express()
+
+app.get('/', (req, res) => {
+  (async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
+    const url = 'https://google.com'
+    const page = await browser.newPage()
+    await page.goto(url, { waitUntil: 'networkidle2' })
+    const buffer = await page.screenshot({path: 'example.png'})
+    res.type('image/png').send(buffer)
+  })()
+})
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  // console.log('Hello world listening on port', port)
+})
